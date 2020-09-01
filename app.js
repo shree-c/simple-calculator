807887a4582c42bda0005e34d7a5e6dc40e5bf22
shree <sammmmmu123@gmail.com>
2020-09-01 21:37:43 +0530

    added clear screen when a button is pressed after equal is pressed

--- app.js
+++ app.js
@@ -1,23 +1,35 @@
 const subscr = document.getElementsByClassName('subscreen')[0];
 
-document.addEventListener('click', (event)=>{
+document.addEventListener('click', (event) => {
     doStuff(event);
 })
-
+let pressedeq = false;
 function doStuff(event) {
-    
+    let insidetext = event.target.innerText;
+
     if (event.target.classList[0] === 'item') {
-        console.log(event.target.innerText)
-        if (event.target.innerText === 'C') {
+        if (insidetext === 'C') {
             subscr.innerText = '';
         }
-        if (event.target.innerText === '=') {
-            subscr.innerText = String(`= ${eval(subscr.innerText)}`) 
+        if (insidetext === '=') {
+            subscr.innerText = String(`= ${eval(subscr.innerText).toFixed(4)}`);
+            pressedeq = true;
         }
-        if (event.target.innerText !== 'C' && event.target.innerText !== '=') {
-            subscr.innerText += event.target.innerText;
+        if (insidetext !== 'C' && insidetext !== '=' && pressedeq) {
+            console.log('came');
+            subscr.innerText = '';
+            pressedeq = false;
+            // if (pressedeq) {
+            //     
+            // } else {
+            //     subscr.innerText += insidetext;
+            // }
 
         }
+        if (insidetext !== 'C' && insidetext !== '=') {
+            subscr.innerText += insidetext;
+        }
+
     }
 
 }
